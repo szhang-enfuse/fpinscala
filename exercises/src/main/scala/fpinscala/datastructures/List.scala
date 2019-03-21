7370bf04d32d4faab9666ec8c1c3b3e3247b1097
Simon Zhang <sizhang@corelogic.com>
2019-03-20 18:21:22 -0700

    learn sth in list

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,6 @@
 package fpinscala.datastructures
 
+import fpinscala.datastructures.List.dropWhile
 import fpinscala.gettingstarted.MyModule.formatAbs
 
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
@@ -11,9 +12,16 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List { // `List` companion object. Contains functions for creating and working with lists.
   def main(args: Array[String]): Unit = {
-    val l = List(1, 2, 3, 4)
-    println(drop(l, 2))
     println(x)
+    val l:List[Int] = List(1, 2, 3, 4,5,-3,6,9)
+    println(drop(l, 2))
+    println(dropWhile(l, (x:Int) => x < 4 ))
+    println(init(l))
+    val d = List(1.0,2.1,3.0,0.0,4.2,5.4)
+    println(product3(d))
+    println(foldRight(l,Nil:List[Int])(Cons(_,_)))
+    println(length(l))
+    println(sum4(l))
   }
 
   def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
@@ -57,6 +65,13 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  def product3(ns: List[Double]) = ns match {
+    case Cons(0.0,t) => {
+      println("found 0.0")
+      0.0
+    }
+    case _ => foldRight (ns, 1.0) (_* _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+  }
 
   def tail[A](l: List[A]): List[A] = l match {
     case Nil => Nil
@@ -79,15 +94,25 @@ object List { // `List` companion object. Contains functions for creating and wo
   }
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case Nil => Nil
-    case Cons(head, tail) => if (f(head)) dropWhile(tail,f) //else Cons(head,dropWhile(tail,f))
+    case Cons(h, t)  if f(h) => dropWhile(t,f)
+    case _ => l
   }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h,Nil) => Nil
+    case Cons(h,t) => Cons(h,init(t))
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l,0) ((_,y)=> y+1)
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case  Cons(x, xs) => foldLeft(xs,f(z,x))(f)
+  }
+  def sum4(ns: List[Int]) =
+    foldLeft(ns, 0)((x,y) => x + y)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
